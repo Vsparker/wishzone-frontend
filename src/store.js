06d034eb337739083795757db9559b1db48caa6b
Vsparker <Victor.parkx@gmail.com>
2021-03-09 15:22:50 -0500

    added order screen

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 
@@ -21,7 +22,9 @@ const reducer = combineReducers({
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
 	userSignin: userSigninReducer,
-	userRegister: userRegisterReducer
+	userRegister: userRegisterReducer,
+	orderCreate: orderCreateReducer,
+	orderDetails: orderDetailsReducer
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
